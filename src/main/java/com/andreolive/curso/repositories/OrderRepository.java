cf82c6320e23c6fd1b3e4b995f4bb362b3cfea0a
andreolive <andreolive@live.com>
2020-12-12 10:51:16 -0300

    Order, Instant, ISO 8601 Parte 2

---
+++ src/main/java/com/andreolive/curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.andreolive.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andreolive.curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
