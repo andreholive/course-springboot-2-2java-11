80f1d49eb78bbe13dd631faa9e5514a51b11e10d
andreolive <andreolive@live.com>
2020-12-12 08:54:18 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/andreolive/curso/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.andreolive.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.andreolive.curso.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
